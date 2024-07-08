35b4ea5235364f4a13a3e5f80fcb1817ef487115
Otto Hooper <votton.hooper@gmail.com>
2024-07-08 22:44:51 +0100

    History list basics

---
+++ force-app/main/default/lwc/historyList/historyList.js
@@ -0,0 +1,71 @@
+import { LightningElement, wire, track } from 'lwc';
+import getHistoryItems from '@salesforce/apex/HistoryController.getHistoryItems';
+import getHistoryVersions from '@salesforce/apex/HistoryController.getHistoryVersions';
+
+export default class HistoryList extends LightningElement {
+    @track historyItems;
+
+    @wire(getHistoryItems)
+    wiredHistoryItems({ error, data }) {
+        if (data) {
+            this.historyItems = data.map(item => ({ 
+                ...item, 
+                isExpanded: false, 
+                versions: [], 
+                hasVersions: false,
+                iconName: this.getIconName(false)
+            }));
+        } else if (error) {
+            // Handle error
+        }
+    }
+
+    getIconName(isExpanded) {
+        return isExpanded ? "utility:chevrondown" : "utility:chevronright";
+    }
+
+    toggleRow(event) {
+        const itemId = event.currentTarget.dataset.id;
+        this.historyItems = this.historyItems.map(item => {
+            if (item.Id === itemId) {
+                item.isExpanded = !item.isExpanded;
+                item.iconName = this.getIconName(item.isExpanded);
+                if (item.isExpanded && item.versions.length === 0) {
+                    this.loadVersions(item);
+                }
+            }
+            return item;
+        });
+    }
+
+    loadVersions(item) {
+        getHistoryVersions({ historyItemId: item.Id })
+            .then(versions => {
+                item.versions = versions;
+                item.hasVersions = versions.length > 0;
+                this.historyItems = [...this.historyItems];
+            })
+            .catch(error => {
+                // Handle error
+            });
+    }
+
+    handleAdd() {
+        // Handle add action
+    }
+
+    handleViewEdit(event) {
+        const itemId = event.currentTarget.dataset.id;
+        // Handle view/edit action
+    }
+
+    handleForward(event) {
+        const itemId = event.currentTarget.dataset.id;
+        // Handle forward to mail recipient action
+    }
+
+    handleDelete(event) {
+        const itemId = event.currentTarget.dataset.id;
+        // Handle delete action
+    }
+}
