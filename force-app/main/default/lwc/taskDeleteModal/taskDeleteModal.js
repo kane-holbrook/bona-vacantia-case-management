6708227743cc066a0c3dab391a3fb44388786a47
Otto Hooper <votton.hooper@gmail.com>
2024-07-10 19:46:48 +0100

    Pass the fields from parent to delete modal tasks

--- force-app/main/default/lwc/taskDeleteModal/taskDeleteModal.js
+++ force-app/main/default/lwc/taskDeleteModal/taskDeleteModal.js
@@ -1,3 +1,27 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, api } from 'lwc';
 
-export default class TaskDeleteModal extends LightningElement {}
+export default class TaskDeleteModal extends LightningElement {
+    @api recordId;
+    @api taskName;
+    @api description;
+    @api scheduleCode;
+    @api caseOfficers;
+    @api category;
+    @api priority;
+    @api waitingPeriod;
+    @api dateInserted;
+    @api due;
+    @api document;
+    @api postTray;
+    @api groupCode;
+    @api otherParty;
+
+    handleDelete() {
+        // Logic to handle the task deletion
+        this.dispatchEvent(new CustomEvent('close'));
+    }
+
+    handleCancel() {
+        this.dispatchEvent(new CustomEvent('close'));
+    }
+}
