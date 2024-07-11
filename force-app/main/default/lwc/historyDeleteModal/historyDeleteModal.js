eddb184a3ee9e81fbf41d77c68bb381793aeadc4
Otto Hooper <votton.hooper@gmail.com>
2024-07-11 23:32:16 +0100

    Some display logic for history delete modal

--- force-app/main/default/lwc/historyDeleteModal/historyDeleteModal.js
+++ force-app/main/default/lwc/historyDeleteModal/historyDeleteModal.js
@@ -1,3 +1,20 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track, api } from 'lwc';
 
-export default class HistoryDeleteModal extends LightningElement {}
+export default class HistoryDeleteModal extends LightningElement {
+    @api record;
+    @track date;
+    @track action;
+    @track documentType;
+    @track correspondence
+    @track draft;
+    @track user;
+
+    connectedCallback() {
+        this.date = this.record.Date_Inserted__c || '';
+        this.action = this.record.Action__c || '';
+        this.documentType = this.record.Document_Type__c || '';
+        this.correspondence = this.record.Correspondence__c || '';
+        this.draft = this.record.Draft__c || '';
+        this.user = this.record.User__c || '';
+    }
+}
