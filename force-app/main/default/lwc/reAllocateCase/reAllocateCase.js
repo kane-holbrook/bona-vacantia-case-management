e59c5600f04b41f1736ff24d794e4f3ba2e7dcdd
Otto Hooper <votton.hooper@gmail.com>
2024-08-11 17:05:49 +0100

    Make the case officer input a real-time search for case gov

--- force-app/main/default/lwc/reAllocateCase/reAllocateCase.js
+++ force-app/main/default/lwc/reAllocateCase/reAllocateCase.js
@@ -1,8 +1,11 @@
-import { LightningElement, api, track } from 'lwc';
+import { LightningElement, api, track, wire } from 'lwc';
+import searchUsers from '@salesforce/apex/TaskController.searchUsers';
 
 export default class ReAllocateCase extends LightningElement {
     @track isModalOpen = false;
-    @track newCaseOfficer = '';
+    @track newCaseOfficer = ''; // This will now store the selected officer's name
+    @track searchTerm = '';
+    @track filteredCaseOfficerOptions = [];
 
     @api
     openModal() {
@@ -19,11 +22,42 @@ export default class ReAllocateCase extends LightningElement {
 
     handleSave() {
         const selectedEvent = new CustomEvent('save', {
-            detail: this.newCaseOfficer
+            detail: this.newCaseOfficer // This will pass the officer's name
         });
 
-        console.log('caseofficerevent', selectedEvent);
         this.dispatchEvent(selectedEvent);
         this.closeModal();
+    }
+
+    handleSearchTermChange(event) {
+        this.searchTerm = event.target.value;
+        this.fetchUsers();
+    }
+
+    fetchUsers() {
+        if (this.searchTerm.length > 2) {
+            searchUsers({ searchTerm: this.searchTerm })
+                .then((result) => {
+                    this.filteredCaseOfficerOptions = result.map((user) => {
+                        return { label: user.Name, value: user.Name }; // Both label and value are the officer's name
+                    });
+                })
+                .catch((error) => {
+                    console.error('Error fetching users:', error);
+                });
+        } else {
+            this.filteredCaseOfficerOptions = [];
+        }
+    }
+
+    selectCaseOfficer(event) {
+        const selectedCaseOfficerName = event.currentTarget.dataset.label;
+        this.newCaseOfficer = selectedCaseOfficerName;
+        this.searchTerm = selectedCaseOfficerName;
+        this.filteredCaseOfficerOptions = [];
+    }
+
+    get shouldShowDropdown() {
+        return this.searchTerm && this.searchTerm.length > 2 && this.filteredCaseOfficerOptions.length > 0;
     }
 }
