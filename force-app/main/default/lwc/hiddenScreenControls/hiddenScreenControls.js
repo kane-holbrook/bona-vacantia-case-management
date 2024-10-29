c40dc2bea4bf38b62b490bac72e48d779fc2bd18
Otto Hooper <votton.hooper@gmail.com>
2024-10-29 16:36:24 +0000

    FOIR hidden screen fields save logic and layout

--- force-app/main/default/lwc/hiddenScreenControls/hiddenScreenControls.js
+++ force-app/main/default/lwc/hiddenScreenControls/hiddenScreenControls.js
@@ -13,6 +13,15 @@ import ENTERED_BY_FIELD from '@salesforce/schema/Case_Detail__c.Entered_By__c';
 import DATE_ENTERED_FIELD from '@salesforce/schema/Case_Detail__c.Date_Entered__c';
 import OPEN_CLOSED_FIELD from '@salesforce/schema/Case_Detail__c.Open_Closed__c';
 import BV_FAIS_NO_FIELD from '@salesforce/schema/Case_Detail__c.Bv_Fais_No__c';
+import TIME_OPENED_FIELD from '@salesforce/schema/Case_Detail__c.Time_Opened__c';
+import HOD_APPROVAL_NEEDED_FIELD from '@salesforce/schema/Case_Detail__c.Hod_Approval_Needed__c';
+import APPROVED_FIELD from '@salesforce/schema/Case_Detail__c.Approved__c';
+import COMPLETED_FIELD from '@salesforce/schema/Case_Detail__c.Completed__c';
+import GRANTED_IN_FULL_FIELD from '@salesforce/schema/Case_Detail__c.Granted_In_Full__c';
+import FULL_REFUSAL_FIELD from '@salesforce/schema/Case_Detail__c.Full_Refusal__c';
+import PART_REFUSAL_FIELD from '@salesforce/schema/Case_Detail__c.Part_Refusal__c';
+import INFO_HELD_FIELD from '@salesforce/schema/Case_Detail__c.Info_Held__c';
+import DEBUG_FIELD from '@salesforce/schema/Case_Detail__c.Debug__c';
 
 export default class HiddenScreenControls extends LightningElement {
     @track isModalOpen = false;
@@ -24,6 +33,15 @@ export default class HiddenScreenControls extends LightningElement {
     @track dateEntered = '';
     @track openClosed = '';
     @track bvFaisNo = null;
+    @track timeOpened = '';
+    @track hodApprovalNeeded = false;
+    @track approved = false;
+    @track completed = false;
+    @track grantedInFull = false;
+    @track fullRefusal = false;
+    @track partRefusal = false;
+    @track infoHeld = '';
+    @track debug = false;
 
     @api bvCaseId;
     @api recordTypeId;
@@ -45,6 +63,21 @@ export default class HiddenScreenControls extends LightningElement {
                 this.dateEntered = data.Date_Entered__c;
                 this.openClosed = data.Open_Closed__c;
                 this.bvFaisNo = data.Bv_Fais_No__c;
+            } else if (this.isFoirScreen) {
+                this.enteredBy = data.Entered_By__c;
+                this.dateEntered = data.Date_Entered__c;
+                this.openClosed = data.Open_Closed__c;
+                this.timeOpened = data.Time_Opened__c;
+                this.hodApprovalNeeded = data.Hod_Approval_Needed__c === 'Yes';
+                this.approved = data.Approved__c === 'Yes';
+                this.clearedForPublishing = data.Cleared_For_Publishing__c === 'Yes';
+                this.completed = data.Completed__c === 'Yes';
+                this.published = data.Published__c === 'Yes';
+                this.grantedInFull = data.Granted_In_Full__c === 'Yes';
+                this.fullRefusal = data.Full_Refusal__c === 'Yes';
+                this.partRefusal = data.Part_Refusal__c === 'Yes';
+                this.infoHeld = data.Info_Held__c;
+                this.debug = data.Debug__c === 'Yes';
             } else {
                 this.isClearedForPublishingChecked = data.Cleared_For_Publishing__c === 'Yes';
                 this.isPublishedChecked = data.Published__c === 'Yes';
@@ -84,6 +117,21 @@ export default class HiddenScreenControls extends LightningElement {
             fields[DATE_ENTERED_FIELD.fieldApiName] = this.dateEntered;
             fields[OPEN_CLOSED_FIELD.fieldApiName] = this.openClosed;
             fields[BV_FAIS_NO_FIELD.fieldApiName] = this.bvFaisNo;
+        } else if (this.isFoirScreen) {
+            fields[ENTERED_BY_FIELD.fieldApiName] = this.enteredBy;
+            fields[DATE_ENTERED_FIELD.fieldApiName] = this.dateEntered;
+            fields[OPEN_CLOSED_FIELD.fieldApiName] = this.openClosed;
+            fields[TIME_OPENED_FIELD.fieldApiName] = this.timeOpened;
+            fields[HOD_APPROVAL_NEEDED_FIELD.fieldApiName] = this.hodApprovalNeeded ? 'Yes' : 'No';
+            fields[APPROVED_FIELD.fieldApiName] = this.approved ? 'Yes' : 'No';
+            fields[CLEARED_FOR_PUBLISHING_FIELD.fieldApiName] = this.clearedForPublishing ? 'Yes' : 'No';
+            fields[COMPLETED_FIELD.fieldApiName] = this.completed ? 'Yes' : 'No';
+            fields[PUBLISHED_FIELD.fieldApiName] = this.published ? 'Yes' : 'No';
+            fields[GRANTED_IN_FULL_FIELD.fieldApiName] = this.grantedInFull ? 'Yes' : 'No';
+            fields[FULL_REFUSAL_FIELD.fieldApiName] = this.fullRefusal ? 'Yes' : 'No';
+            fields[PART_REFUSAL_FIELD.fieldApiName] = this.partRefusal ? 'Yes' : 'No';
+            fields[INFO_HELD_FIELD.fieldApiName] = this.infoHeld;
+            fields[DEBUG_FIELD.fieldApiName] = this.debug ? 'Yes' : 'No';
         } else {
             fields[CLEARED_FOR_PUBLISHING_FIELD.fieldApiName] = this.isClearedForPublishingChecked ? 'Yes' : 'No';
             fields[PUBLISHED_FIELD.fieldApiName] = this.isPublishedChecked ? 'Yes' : 'No';
@@ -123,6 +171,10 @@ export default class HiddenScreenControls extends LightningElement {
     }
 
     get isEstatesOrSimilarScreen() {
-        return ['ESTA', 'COMP', 'CONV'].includes(this.recordTypeDeveloperName);
+        return ['ESTA', 'COMP', 'CONV', 'GENE'].includes(this.recordTypeDeveloperName);
+    }
+
+    get isFoirScreen() {
+        return this.recordTypeDeveloperName === 'FOIR';
     }
 }
