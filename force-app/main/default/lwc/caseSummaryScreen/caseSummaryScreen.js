f97b3ee17bff386c10337b5d010f66418d6eab71
Otto Hooper <votton.hooper@gmail.com>
2024-11-05 12:17:32 +0000

    Display real data for case summary fields

--- force-app/main/default/lwc/caseSummaryScreen/caseSummaryScreen.js
+++ force-app/main/default/lwc/caseSummaryScreen/caseSummaryScreen.js
@@ -1,5 +1,57 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, api, wire } from 'lwc';
+import getCaseSummaryData from '@salesforce/apex/CaseSummaryController.getCaseSummaryData';
 
 export default class CaseSummaryScreen extends LightningElement {
-    // Future properties for dynamic data can be added here
+    @api recordId;
+
+    keyName = '';
+    keyAddress = '';
+    caseType = '';
+    caseOfficer = '';
+    dateOpened = '';
+    firstActionDate = '';
+    allocatedDate = '';
+    onReportTo = '';
+    referredTo = '';
+    referredToDate = '';
+    lastActionDate = '';
+    currentStatus = '';
+    formerRef = '';
+    detailsComplete = '';
+    dateCaseLastReviewed = '';
+    dateOfLastLmReview = '';
+    dateOfLastCoReview = '';
+
+    @wire(getCaseSummaryData, { caseId: '$recordId' })
+    wiredCaseSummary({ error, data }) {
+        if (data) {
+            this.keyName = data.keyName || '';
+            this.keyAddress = data.keyAddress || '';
+            this.caseType = data.caseType || '';
+            this.caseOfficer = data.caseOfficer || '';
+            this.dateOpened = this.formatDate(data.dateOpened);
+            this.firstActionDate = this.formatDate(data.firstActionDate);
+            this.onReportTo = data.onReportTo || '';
+            this.referredTo = data.referredTo || '';
+            this.referredToDate = this.formatDate(data.referredToDate);
+            this.lastActionDate = this.formatDate(data.lastActionDate);
+            this.currentStatus = data.currentStatus || '';
+            this.formerRef = data.formerRef || '';
+            this.detailsComplete = data.detailsComplete || '';
+            this.dateCaseLastReviewed = this.formatDate(data.dateCaseLastReviewed);
+            this.dateOfLastLmReview = this.formatDate(data.dateOfLastLmReview);
+            this.dateOfLastCoReview = this.formatDate(data.dateOfLastCoReview);
+        } else if (error) {
+            console.error(error);
+        }
+    }
+
+    formatDate(dateString) {
+        if (!dateString) return '';
+        const date = new Date(dateString);
+        const day = String(date.getDate()).padStart(2, '0');
+        const month = String(date.getMonth() + 1).padStart(2, '0'); // Months are zero-based
+        const year = date.getFullYear();
+        return `${day}/${month}/${year}`;
+    }
 }
