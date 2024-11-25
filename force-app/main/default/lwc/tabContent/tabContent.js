f1d30cfdebdc86dfec7c0e157bc2de155ba52f5d
Otto Hooper <votton.hooper@gmail.com>
2024-11-25 22:56:48 +0000

    If case type CONV, hide case summary tab as use the DB screen

--- force-app/main/default/lwc/tabContent/tabContent.js
+++ force-app/main/default/lwc/tabContent/tabContent.js
@@ -1,6 +1,27 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, wire } from 'lwc';
+import getRecordTypeDeveloperName from '@salesforce/apex/LayoutController.getRecordTypeDeveloperName';
+import getRecordTypeIdForRecord from '@salesforce/apex/LayoutController.getRecordTypeIdForRecord';
+import { getRecordId } from 'c/sharedService';
 
 export default class TabContent extends LightningElement {
+    recordId;
+    isConv = false;
+
+    connectedCallback() {
+        this.recordId = getRecordId();
+        this.retrieveRecordTypeDeveloperName();
+    }
+
+    async retrieveRecordTypeDeveloperName() {
+        try {
+            const recordTypeId = await getRecordTypeIdForRecord({ recordId: this.recordId });
+            const recordTypeDeveloperName = await getRecordTypeDeveloperName({ recordTypeId });
+            this.isConv = recordTypeDeveloperName === 'CONV';
+        } catch (error) {
+            console.error('Error retrieving record type developer name:', error);
+        }
+    }
+
     handleFlowFinished() {
         const caseOfficerHistory = this.template.querySelector('c-case-officer-history');
         if (caseOfficerHistory) {
